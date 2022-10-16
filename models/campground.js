8ff918badd52237cdaafb40fe952aa2ac422281f
Mohammed Ahbar Ajaib <ahbarajaib@gmail.com>
2022-10-16 15:19:27 +0530

    490 complete

--- models/campground.js
+++ models/campground.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const Review = require("./review");
 const CampgroundSchema = new Schema({
   title: String,
   image: String,
@@ -12,6 +13,15 @@ const CampgroundSchema = new Schema({
       ref: "Review",
     },
   ],
+});
+CampgroundSchema.post("findOneAndDelete", async function (doc) {
+  if (doc) {
+    await Review.remove({
+      _id: {
+        $in: doc.reviews,
+      },
+    });
+  }
 });
 
 module.exports = mongoose.model("Campground", CampgroundSchema);
